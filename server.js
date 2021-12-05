727f3ea60a234dc10b7766cf99b8f2c0a30f88ee
calamara <90728300+calamara@users.noreply.github.com>
2021-12-05 21:11:24 +0100

    aggiornamenti client/server

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
+//setup del server
 console.log("hdudhuiehde");
-
 let express = require("express");
-
 let app = express();
-
 let port = 3000;
-
 let server = app.listen(port);
-
 console.log("server is running on http://localhost:" + port);
-
 app.use(express.static("public"));
+//fine setup server
+
+let serverSocket = require("socket.io");
+let io = serverSocket(server);
+
+//
+io.on("connection", newConnection);
+function newConnection(newSocket) {
+  console.log(newSocket.id);
+}
